906946e039007390c3e7cdef6578b141ac999fba
amir gurevich <noamir22@gmail.com>
2022-06-07 09:54:10 +0300

    create all windows and the look of them exept the avatars

--- WpfApp5/JoinRoom.xaml.cs
+++ WpfApp5/JoinRoom.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace WpfApp5;
 
@@ -7,5 +8,10 @@ public partial class JoinRoom : Window
     public JoinRoom()
     {
         InitializeComponent();
+    }
+
+    private void Check_Choosing_Room(object sender, SelectionChangedEventArgs e)
+    {
+        throw new System.NotImplementedException();
     }
 }
