44254efe5c1ae9aa5ba480765be5f33141bc9d21
amir gurevich <noamir22@gmail.com>
2022-06-07 18:37:14 +0300

    aaaaaaaaaa help me

--- WpfApp5/CreateRoom.xaml.cs
+++ WpfApp5/CreateRoom.xaml.cs
@@ -1,11 +1,52 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 
 namespace WpfApp5;
 
 public partial class CreateRoom : Window
 {
+    private bool _flag = true;
     public CreateRoom()
     {
         InitializeComponent();
+    }
+
+    private void Back_OnClick(object sender, RoutedEventArgs e)
+    {
+        // when the user click the sign up button, the animation will be play.
+        // when the animation is completed the window will be closed and the sign up window will be opened
+        Sb.Completed += new EventHandler(move_to_menu); // add the event handler to the completed event of the animation
+        Sb.Begin(); // start the animation
+
+    }
+
+    private void move_to_menu(object? sender, EventArgs e)
+    {
+        // the animation will not be play again
+        if (!_flag) return;
+        _flag = false;
+
+        Menu window = new Menu(); // create a new window
+        this.Close(); // close the current window
+        window.Show(); // show the new window
+    }
+
+    private void Create_OnClick(object sender, RoutedEventArgs e)
+    {
+        // when the user click the sign up button, the animation will be play.
+        // when the animation is completed the window will be closed and the sign up window will be opened
+        Sb.Completed += new EventHandler(move_to_waiting_room_host); // add the event handler to the completed event of the animation
+        Sb.Begin(); // start the animation
+    }
+
+    private void move_to_waiting_room_host(object? sender, EventArgs e)
+    {
+        // the animation will not be play again
+        if (!_flag) return;
+        _flag = false;
+
+        WaitingRoomHost window = new WaitingRoomHost(); // create a new window
+        this.Close(); // close the current window
+        window.Show(); // show the new window
     }
 }
--- WpfApp5/JoinRoom.xaml.cs
+++ WpfApp5/JoinRoom.xaml.cs
@@ -8,6 +8,7 @@ namespace WpfApp5;
 public partial class JoinRoom : Window
 {
     private List<string> _rooms = new List<string>();
+    private bool _flag = true;
 
     public JoinRoom()
     {
@@ -89,5 +90,25 @@ public partial class JoinRoom : Window
     {
         // enable the join button if the user choose a room
         this.joinRoom.IsEnabled = this.RoomsList.SelectedIndex >= 0;
+    }
+
+    private void Back_OnClick(object sender, RoutedEventArgs e)
+    {
+        // when the user click the sign up button, the animation will be play.
+        // when the animation is completed the window will be closed and the sign up window will be opened
+        Sb.Completed += new EventHandler(move_to_menu); // add the event handler to the completed event of the animation
+        Sb.Begin(); // start the animation
+
+    }
+
+    private void move_to_menu(object? sender, EventArgs e)
+    {
+        // the animation will not be play again
+        if (!_flag) return;
+        _flag = false;
+
+        Menu window = new Menu(); // create a new window
+        this.Close(); // close the current window
+        window.Show(); // show the new window
     }
 }
