f7dc35f9643d0b11807d20923f40fd20ce100ec7
amir gurevich <noamir22@gmail.com>
2022-06-07 10:09:16 +0300

    add commit to Login.xaml.cs

--- WpfApp5/Login.xaml.cs
+++ WpfApp5/Login.xaml.cs
@@ -1,58 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-// using System.Media;
+
 
 namespace WpfApp5;
 
 public partial class Login : Window
 {
     private bool _flag = true;
+
     public Login()
     {
         InitializeComponent();
-
-        //
-        // SoundPlayer player = new SoundPlayer();
-        // player.SoundLocation = AppDomain.CurrentDomain.BaseDirectory + "lobby-classic-game.wav";
-        // player.Play();
     }
 
     private void SighUp_OnClick(object sender, RoutedEventArgs e)
+
     {
-        Sb.Completed += new EventHandler(move_to_signup);
-        Sb.Begin();
+        // when the user click the sign up button, the animation will be play.
+        // when the animation is completed the window will be closed and the sign up window will be opened
+        Sb.Completed += new EventHandler(move_to_signup); // add the event handler to the completed event of the animation
+        Sb.Begin(); // start the animation
     }
 
     private void move_to_signup(object? sender, EventArgs e)
     {
+        // the animation will not be play again
         if (!_flag) return;
         _flag = false;
-        SighUp window = new SighUp();
-        this.Close();
-        window.Show();
+
+        SighUp window = new SighUp(); // create a new window
+        this.Close(); // close the current window
+        window.Show(); // show the new window
     }
+
     private void Quit_OnClick(object sender, RoutedEventArgs e)
     {
         // TODO: send exit to server 
-        Sb.Completed += new EventHandler(Sb_Completed);
-        Sb.Begin();
+        // when the user click the quit button, the animation will be play.
+        // when the animation is completed the window will be closed.
+        Sb.Completed += new EventHandler(Sb_Completed); // add the event handler to the completed event of the animation
+        Sb.Begin(); // start the animation
     }
+
     private void Sb_Completed(object? sender, EventArgs e)
     {
-        this.Close();
+        this.Close(); // close the current window
     }
 
     private void Login_OnClick(object sender, RoutedEventArgs e)
@@ -60,16 +52,17 @@ public partial class Login : Window
         string username = new string(this.Username.Text);
         string password = new string(this.Password.Password.ToString());
         // TODO: login to the server
-        Sb.Completed += new EventHandler(move_to_menu);
-        Sb.Begin();
+        // TODO: if the login is successful, the window will be closed and the menu window will be opened
+        Sb.Completed += new EventHandler(move_to_menu); // add the event handler to the completed event of the animation
+        Sb.Begin(); // start the animation
     }
 
     private void move_to_menu(object? sender, EventArgs e)
     {
-        if (!_flag) return;
+        if (!_flag) return; // the animation will not be play again
         _flag = false;
-        Menu window = new Menu();
-        this.Close();
-        window.Show();
+        Menu window = new Menu(); // create a new window
+        this.Close(); // close the current window
+        window.Show(); // show the new window
     }
 }
